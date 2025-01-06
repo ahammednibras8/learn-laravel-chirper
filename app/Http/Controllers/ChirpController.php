ccd97ed0555e6ccb0b30274dd58bc0dc65c214e9
ahammednibras8 <ahammednibras@proton.me>
2025-01-06 07:44:55 +0530

    update the edit method on our ChirpController to display our form

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\RedirectResponse;
 use App\Models\Chirp;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Gate;
 
